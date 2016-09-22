4806f7f28c5701104ea2568e9b4cf5e0e2fd592f
Anders <anders.stensby.widgren@ciber.no>
2016-09-22 09:28:13 +0200

    removed variable from request

--- src/main/java/timeforing/controllers/EmployeeController.java
+++ src/main/java/timeforing/controllers/EmployeeController.java
@@ -1,15 +1,14 @@
 package timeforing.controllers;
 
-import timeforing.domain.Employee;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import timeforing.domain.Employee;
 
 @RestController
 public class EmployeeController {
 
     @RequestMapping("/employee")
-    public Employee getEmployee(@RequestParam int employeeId){
+    public Employee getEmployee(){
         return new Employee("3145", 23);
     }
 
